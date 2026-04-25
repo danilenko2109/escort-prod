3dfbda826b52dc70c30abf8eefb45d85dc3d1ae5
danilenko2109 <bohdan.danilenko@gmail.com>
2026-04-25 13:44:27 +0200

    Change API URL to production endpoint

--- frontend/src/services/api/index.js
+++ frontend/src/services/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8001";
+const API_URL ="https://escort-prod.onrender.com";
 
 const apiClient = axios.create({
   baseURL: API_URL,
